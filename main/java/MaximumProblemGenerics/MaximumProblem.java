b31ba55bd8bc49206ee11778e03e8cd43ab3072a
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-10-04 01:31:58 +0530

    Add master

---
+++ main/java/MaximumProblemGenerics/MaximumProblem.java
@@ -0,0 +1,8 @@
+package MaximumProblemGenerics;
+
+public class MaximumProblem {
+	public static void main(String[] args) {
+		System.out.println("Welcome to generics code");
+	}
+
+}
---
+++ test/java/MaximumProblemGenerics/MaximumProblemTest.java
@@ -0,0 +1,5 @@
+package MaximumProblemGenerics;
+
+public class MaximumProblemTest {
+
+}
