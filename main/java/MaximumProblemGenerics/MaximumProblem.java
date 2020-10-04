91f717efeb7fd2810c4b58d58343bb91358e4ca8
Dhanshree123 <2016181@iiitdmj.ac.in>
2020-10-04 15:47:08 +0530

    Add RefUC1

--- main/java/MaximumProblemGenerics/MaximumProblem.java
+++ main/java/MaximumProblemGenerics/MaximumProblem.java
@@ -1,34 +1,18 @@
 package MaximumProblemGenerics;
 
 public class MaximumProblem {
-	public Integer findMaximum(Integer a,Integer b,Integer c) {
-		Integer max = a;
-		if(b.compareTo(max) >0)
-			max =b;
-		if(c.compareTo(max) >0)
-			max = c;
-		
-		return max;
-	}
 	
-	public Float findMaximum(Float a,Float b,Float c) {
-		Float max = a;
-		if(b.compareTo(max) >0)
-			max =b;
-		if(c.compareTo(max) >0)
-			max = c;
-		
-		return max;
-	}
 	
-	public String findMaximum(String a,String b,String c) {
-		String max = a;
+	public <E extends Comparable<E>> E findMaximum(E a,E b,E c) {
+		E max = a;
 		if(b.compareTo(max) >0)
 			max =b;
 		if(c.compareTo(max) >0)
 			max = c;
 		
 		return max;
+		
 	}
+
 
 }
